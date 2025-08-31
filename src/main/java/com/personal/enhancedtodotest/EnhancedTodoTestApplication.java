abe9c157e1b27aee0bec12044483962a2ace4926
Rahat <rahatakhter4@gmail.com>
2025-08-31 18:10:30 +0600

    first commit

---
+++ src/main/java/com/personal/enhancedtodotest/EnhancedTodoTestApplication.java
@@ -0,0 +1,13 @@
+package com.personal.enhancedtodotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancedTodoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancedTodoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/enhancedtodotest/EnhancedTodoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.enhancedtodotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedTodoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
