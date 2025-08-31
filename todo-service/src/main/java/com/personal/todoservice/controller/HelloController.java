4e57208388b3a16da2feee6fe277f9078bf2e186
Rahat <rahatakhter4@gmail.com>
2025-09-01 01:14:15 +0600

    created required modules

---
+++ todo-service/src/main/java/com/personal/todoservice/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.personal.todoservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/todo-service")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Todo Service!";
+    }
+}
