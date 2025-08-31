7091ff8979f90c893bbc655ede1cbcac0688a334
Rahat <rahatakhter4@gmail.com>
2025-08-31 23:46:48 +0600

    added separate modules

--- api-gateway/src/main/java/com/personal/todoservice/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/personal/todoservice/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.personal.enhancedtodotest;
+package com.personal.todoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnhancedTodoTestApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EnhancedTodoTestApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
