4ae1176c5c58d20425e090ba9a0069a5fe013c41
Rahat <rahatakhter4@gmail.com>
2025-09-01 16:45:27 +0600

    refactored all servers

--- todo-service/src/main/java/com/personal/api_gateway/TodoServiceApplication.java
+++ todo-service/src/main/java/com/personal/api_gateway/TodoServiceApplication.java
@@ -1,4 +1,4 @@
-package com.personal.todoservice;
+package com.personal.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
