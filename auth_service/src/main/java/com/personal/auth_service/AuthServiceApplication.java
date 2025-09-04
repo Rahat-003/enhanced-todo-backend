fb4820222e874b67fcbb21cf1d5570a914c7e958
Rahat <rahatakhter4@gmail.com>
2025-09-05 01:17:24 +0600

    login, registration done

--- auth_service/src/main/java/com/personal/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/com/personal/auth_service/AuthServiceApplication.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EntityScan(basePackages = {"com.personal.domain"})
 @EnableJpaRepositories(basePackages = {"com.personal.repository"})
@@ -20,6 +23,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
 
