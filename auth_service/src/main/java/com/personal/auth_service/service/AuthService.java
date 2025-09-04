4c06e4598d7ccfedd246943f163b2f5aef6e55c1
Rahat <rahatakhter4@gmail.com>
2025-09-04 18:01:12 +0600

    login via email

--- auth_service/src/main/java/com/personal/auth_service/service/AuthService.java
+++ auth_service/src/main/java/com/personal/auth_service/service/AuthService.java
@@ -25,46 +25,30 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     public AuthResponse register(RegisterRequest request) {
-        log.info("Attempting to register new user with username: {}", request.getUserName());
-        // Check if username already exists
-        if (appUserRepository.findByUserName(request.getUserName()).isPresent()) {
-            log.warn("Username {} already exists", request.getUserName());
-            throw new UserAlreadyExistsException("Username already exists");
-        }
-
-        // Check if email already exists
         if (appUserRepository.findByEmail(request.getEmail()).isPresent()) {
             log.warn("Email {} already exists", request.getEmail());
             throw new UserAlreadyExistsException("Email already exists");
         }
-
-        // Create new user
 
         AppUser user = new AppUser();
         user.setUserName(request.getUserName());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-        // Save user
         appUserRepository.save(user);
         log.info("User {} registered successfully", user.getUserName());
 
-        // Generate JWT token
         return generateAuthResponse(user.getUserName());
     }
 
     public AuthResponse login(AuthRequest request) {
-        log.info("Attempting to login user with username: {}", request.getUserName());
-        // Find user by username
-        AppUser user = appUserRepository.findByUserName(request.getUserName())
+        AppUser user = appUserRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> {
-                    log.warn("User not found with username: {}", request.getUserName());
+                    log.warn("User not found with email: {}", request.getEmail());
                     return new UserNotFoundException("User not found");
                 });
-
-        // Verify password
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            log.warn("Invalid credentials for user: {}", request.getUserName());
+            log.warn("Invalid credentials for user: {}", request.getEmail());
             throw new InvalidCredentialsException("Invalid credentials");
         }
 
