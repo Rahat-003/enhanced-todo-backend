88bb9935efb9e79c8639b68f0d7d92d2ece0695a
Rahat <rahatakhter4@gmail.com>
2025-09-07 12:49:35 +0600

    synced module

--- todo-service/src/main/java/com/personal/todo_service/TodoServiceApplication.java
+++ todo-service/src/main/java/com/personal/todo_service/TodoServiceApplication.java
@@ -1,4 +1,4 @@
-package com.personal.gateway;
+package com.personal.todo_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- todo-service/src/main/java/com/personal/todo_service/controller/HelloController.java
+++ todo-service/src/main/java/com/personal/todo_service/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.personal.gateway.controller;
+package com.personal.todo_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
