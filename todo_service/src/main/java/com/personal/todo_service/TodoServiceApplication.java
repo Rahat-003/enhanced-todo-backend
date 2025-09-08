0b42719fc55394d865be1b2712a2dfafba32c20c
Rahat <rahatakhter4@gmail.com>
2025-09-08 22:37:50 +0600

    todo_service fetching config from home directory

--- todo_service/src/main/java/com/personal/todo_service/TodoServiceApplication.java
+++ todo_service/src/main/java/com/personal/todo_service/TodoServiceApplication.java
@@ -1,9 +1,22 @@
 package com.personal.todo_service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EntityScan(basePackages = {"com.personal.domain"})
+@EnableJpaRepositories(basePackages = {"com.personal.repository"})
+@ComponentScan(basePackages = {
+        "com.personal.auth_service",
+        "com.personal.repository",
+        "com.personal.service"
+})
 @SpringBootApplication
+@RequiredArgsConstructor
 public class TodoServiceApplication {
 
     public static void main(String[] args) {
